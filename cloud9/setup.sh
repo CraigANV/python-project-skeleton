e2b493e680f30b67a2edbb111e16934ba1a63ba1
Craig Carey <cynitec@gmail.com>
2020-08-19 15:39:46 +0100

    Cloud9 debugging, matplotlib test

--- cloud9/setup.sh
+++ cloud9/setup.sh
@@ -3,4 +3,7 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p "$HOME/miniconda3"
 
+# Required for debugging in Cloud9, not available via Conda
+pip3 install ikp3db
+
 echo 'export PATH="$PATH:$HOME/miniconda3/bin/"' >> "$HOME/.bashrc"
